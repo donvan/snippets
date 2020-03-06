a4cc3b95072cf14cf69bc495df10143108cef164
donvan <donvan@yeah.net>
2020-03-06 23:05:07 +0800

    递归组件

---
+++ recursion_com.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="list-item" v-for="(item, index) in lists" :key="index">
+            <div class="item-name">
+                <span>{{item.name}}</span>
+            </div>
+            <div class="children-item" v-if="item.children">
+                <list :list="item.children"></list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"List", //递归调用
+    data(){
+        lists:[{
+            name:'level1-1',
+            children:[{
+                name:'level1-2',
+                children:[{
+                    name:'level1-3'
+                }]
+            },
+            ]
+        }]
+    },
+}
+</script>
