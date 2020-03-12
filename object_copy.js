2577bc49c9a9ca299f6eb03193dccfbb885be4c0
donvan <donvan@yeah.net>
2020-03-12 16:04:42 +0800

    对象拷贝

---
+++ object_copy.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = function (target, sources) {
+    var n, source, key;
+
+    for (n = 1; n < arguments.length; n++) {
+        source = arguments[n];
+
+        for (key in source) {
+            if (source.hasOwnProperty(key)) {
+                target[key] = source[key]
+            }
+        }
+    }
+}
