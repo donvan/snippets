e6cd726b826203ba34ca11dfa0b97ead0d498de2
donvan <donvan@yeah.net>
2020-03-12 16:05:13 +0800

    计算属性处理vuex state属性

---
+++ vuex_form.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input v-model="message">
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+
+    },
+    computed:{
+        msg:{ //计算属性的set来改变vuex中的属性
+            get(){
+                return this.$store.state.obj.message
+            },
+            set(value){
+                this.$store.commit('updataMessage',value)
+            }
+        }
+    }
+}
+</script>
